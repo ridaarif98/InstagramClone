18b2fcd08b65267bdb3f4eed164cc2111e53906f
ridaarif98 <ridaarif16@gmail.com>
2022-01-11 18:08:49 +0100

    Add user image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
     
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name,:user_name, :email, :password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name,:user_name, :image, :email, :password, ) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:full_name, :email, :password, :current_password) }
   end
 end
