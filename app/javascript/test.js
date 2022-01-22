ecce69d525660c8fddadee4bf3176106bd1414dd
ridaarif98 <ridaarif16@gmail.com>
2022-01-22 20:47:42 +0100

    Add script for image preview

---
+++ app/javascript/test.js
@@ -0,0 +1,22 @@
+ 
+
+    // console.log("file changed");
+    // function readURL(input) {
+
+    //     if (input.files && input.files[0]) {
+    //         var reader = new FileReader();
+    
+    //         reader.onload = function (e) {
+    //             $('#blah').attr('src', e.target.result);
+    //         }
+    
+    //         reader.readAsDataURL(input.files[0]);
+    //     }
+    // }
+
+    // $("body").on('change',"#imgUp", function() {
+    //     console.log('clicked');
+    // })
+//  function test() {
+//         console.log('clicked');
+// }
