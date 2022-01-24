46f47d054d78d7edab73899fb659ba3fcbf82931
ridaarif98 <ridaarif16@gmail.com>
2022-01-24 14:31:42 +0100

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,26 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.where(user_id: current_user.id)
+  end
 
-    def index
-        @posts = Post.where(user_id: current_user.id);
-    end
+  def new
+    @user = current_user
+    @post = Post.new
+  end
 
-    def new
-        @user = current_user
-        @post = Post.new
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = 'Post created successfully.'
+      redirect_to root_path
+    else
+      render :new
     end
+  end
 
-    def create
-        @post = Post.new(post_params)
-        if @post.save
-            flash[:notice] = 'Post created successfully.'
-            redirect_to root_path
-        else
-            render :new
-        end 
-    end
+  private
 
-    private
-    def post_params
-        params.require(:post).permit(:text, :image, :user_id)
-    end
+  def post_params
+    params.require(:post).permit(:text, :image, :user_id)
+  end
 end
