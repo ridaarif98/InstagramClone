c1a9c54150493ab7fb5ab3310e14c77429ad1f64
ridaarif98 <ridaarif16@gmail.com>
2022-02-02 17:58:15 +0100

    Set route for likes and create method to add and remove likes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
 
+  get 'post/like/:post_id' => 'likes#save_likes', as: :like_post
+
     resources :users, only: [:index, :show]
     resources :posts, only: [:index, :show, :new, :create, :destroy]
 end
