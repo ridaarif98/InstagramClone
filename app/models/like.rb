62dfc00021112a616764c48bfa033f91fa70f156
ridaarif98 <ridaarif16@gmail.com>
2022-02-02 18:00:47 +0100

    Fix linter errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,17 +1,16 @@
 class Like < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
+  belongs_to :user
+  belongs_to :post
 
-    validates_uniqueness_of :post_id, scope: :user_id
-    after_create :update_like_counter
-    after_destroy :decrement_like_counter
+  validates_uniqueness_of :post_id, scope: :user_id
+  after_create :update_like_counter
+  after_destroy :decrement_like_counter
 
-    def update_like_counter
-      Post.find(self.post_id).increment(:likes_counter).save
-    end
-
-    def decrement_like_counter
-      Post.find(self.post_id).decrement(:likes_counter).save
-    end
+  def update_like_counter
+    Post.find(post_id).increment(:likes_counter).save
   end
-  
+
+  def decrement_like_counter
+    Post.find(post_id).decrement(:likes_counter).save
+  end
+end
