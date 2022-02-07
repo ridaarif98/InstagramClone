6d77dc524466a3718cb78a1c2410a4b44f1602d5
ridaarif98 <ridaarif16@gmail.com>
2022-02-07 16:59:39 +0100

    Fix linter errors

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,4 +1,4 @@
 class Follower < ApplicationRecord
-    # belongs_to :user
-    validates_uniqueness_of :follower_id, scope: :following_id
+  # belongs_to :user
+  validates_uniqueness_of :follower_id, scope: :following_id
 end
