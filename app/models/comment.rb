f7f2df750b5bb684a481253e4ea7129ee9dc441e
ridaarif98 <ridaarif16@gmail.com>
2022-02-14 17:02:44 +0100

    Create comment model and set association with other models

---
+++ app/models/comment.rb
@@ -0,0 +1,9 @@
+class Comment < ApplicationRecord
+    belongs_to :user
+    belongs_to :post
+    after_create :update_comment_counter
+
+    def update_comment_counter
+        Post.find(post_id).increment(:comments_counter).save
+    end
+end
