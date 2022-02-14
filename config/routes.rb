8ea9290838be9e2f452c1fdec59a59624405f457
ridaarif98 <ridaarif16@gmail.com>
2022-02-14 17:03:46 +0100

    Add route for comment and fix padding in header

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
 
     resources :users, only: [:index, :show]
     resources :posts, only: [:index, :show, :new, :create, :destroy]
+    resources :comments, only: [:create]
 end
